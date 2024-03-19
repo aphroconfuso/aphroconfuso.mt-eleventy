c4adf0bae2d417d2c5bef3106c2a70e4b11c206e
Joe Gatt <joe@joegatt.net>
2024-03-19 09:05:57 +0100

    Terminu design

--- src/makePageTitle.js
+++ src/makePageTitle.js
@@ -2,8 +2,9 @@ const fixSubjectDate = require("../src/fixSubjectDate.js");
 
 module.exports = (title, author, translator, sequenceTitle, sequenceNumber, subjectDate, sequenceEpisodeTitle, type) => {
 	return [
-		subjectDate ? (type === 'Djarju' ? 'Djarju ' : 'Poddata') : sequenceTitle,
-		subjectDate && fixSubjectDate(subjectDate) || sequenceNumber && (`#${sequenceNumber}`),
+		subjectDate ? type : '',
+		sequenceTitle,
+		subjectDate && fixSubjectDate(subjectDate) || sequenceNumber && (`#${ sequenceNumber }`),
 		sequenceEpisodeTitle || title,
 		"ta’",
 		author,
--- src/makeTitleSlug.js
+++ src/makeTitleSlug.js
@@ -1,12 +1,15 @@
 const fixSubjectDate = require("./fixSubjectDate.js");
 const slugifyMaltese = require("./slugifyMaltese.js");
 
-module.exports = (title, author, translator, sequenceTitle, sequenceNumber, subjectDate, sequenceEpisodeTitle, type) => slugifyMaltese(
-	`
-		${ author || '' }
-		${ translator || '' }
-		${ !!subjectDate ? (type === 'Djarju' ? 'djarju' : 'poddata') : (sequenceTitle || '') }
-		${ (!!subjectDate &&  subjectDate.replace(/\./g, '-')) || sequenceNumber || '' }
-		${ sequenceEpisodeTitle || title }
-	`
-);
+module.exports = (title, author, translator, sequenceTitle, sequenceNumber, subjectDate, sequenceEpisodeTitle, type) => {
+	if (type === 'Terminu') return slugifyMaltese(`il-kliem-fit-teorija-${title}`);
+	return slugifyMaltese(
+		`
+			${ author || '' }
+			${ translator || '' }
+			${ !!subjectDate ? (type === 'Djarju' ? 'djarju' : 'poddata') : (sequenceTitle || '') }
+			${ (!!subjectDate &&  subjectDate.replace(/\./g, '-')) || sequenceNumber || '' }
+			${ sequenceEpisodeTitle || title }
+		`
+	);
+}
