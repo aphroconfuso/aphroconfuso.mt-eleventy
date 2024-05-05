fd38faae7e6c801c959999c46eb101aa080c6a6c
Joe Gatt <joe@joegatt.net>
2024-05-05 10:00:39 +0200

    Update home layouts

--- _data/homepage.js
+++ _data/homepage.js
@@ -117,6 +117,20 @@ async function getHomepage() {
 			text: 11,
 			lengths: [2150, 1200],
 		},
+		Layout_3: {
+			diary: 2,
+			image: 2,
+			poem: 1,
+			text: 11,
+			lengths: [2150, 1200],
+		},
+		Layout_4: {
+			diary: 2,
+			image: 2,
+			poem: 1,
+			text: 11,
+			lengths: [2150, 1200],
+		},
 	}
 
 	const layoutConfig = layouts[atts.layout];
