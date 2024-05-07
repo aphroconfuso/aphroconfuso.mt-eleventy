bb516297dfc9a8d8e07c3ce3c98dca2d0d3edd9e
Joe Gatt <joe@joegatt.net>
2024-05-07 23:38:33 +0200

    Hardcode

--- src/getReads.js
+++ src/getReads.js
@@ -2,8 +2,10 @@ module.exports = (pronoun) => {
 	const reads = {
 		'hi': 'taqra',
 		'hu': 'jaqra',
-		'hi_hu': Math.round(Math.random()) ? 'taqra' : 'jaqra',
+		'hi_hu': 'taqra',
 		'huma': 'jaqraw',
 	}
 	return reads[pronoun || 'huma'];
 }
+
+// 'hi_hu': Math.round(Math.random()) ? 'taqra' : 'jaqra',
