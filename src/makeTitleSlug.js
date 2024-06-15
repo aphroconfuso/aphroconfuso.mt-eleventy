19306e84d950943338c5da778ded0294bf27ba7e
Joe Gatt <joe@joegatt.net>
2024-06-16 01:22:55 +0200

    Temp fix for title slug

--- src/makeTitleSlug.js
+++ src/makeTitleSlug.js
@@ -6,9 +6,13 @@ module.exports = (title, author, translator, sequenceTitle, sequenceNumber, subj
 		`
 			${ author || '' }
 			${ translator || '' }
-			${ !!subjectDate ? (type === 'Djarju' ? 'djarju' : 'poddata') : (sequenceTitle || '') }
-			${ (!!subjectDate &&  subjectDate.replace(/\./g, '-')) || sequenceNumber || '' }
+			${ !!subjectDate && (type === 'Djarju' || type === 'Poddata') ? (type === 'Djarju' ? 'djarju' : 'poddata') : (sequenceTitle || '') }
+			${ (!!subjectDate && (type === 'Djarju' || type === 'Poddata') &&  subjectDate.replace(/\./g, '-')) || sequenceNumber || '' }
 			${ sequenceEpisodeTitle || title }
 		`
 	);
 }
+
+// TEMPORARILY changed these until cms is upgraded to be able to delete dates (4.12.5)
+// ${ !!subjectDate ? (type === 'Djarju' ? 'djarju' : 'poddata') : (sequenceTitle || '') }
+// ${ (!!subjectDate &&  subjectDate.replace(/\./g, '-')) || sequenceNumber || '' }
