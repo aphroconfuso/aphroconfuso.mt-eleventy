888308a14cb25acfb0c1d4aed28715d8aeeb4963
Joe Gatt <joe@joegatt.net>
2024-09-14 19:52:06 +0200

    Fix makePageTitle

--- src/makePageTitle.js
+++ src/makePageTitle.js
@@ -3,12 +3,15 @@ const fixSubjectDate = require("../src/fixSubjectDate.js");
 module.exports = (title, author, translator, sequenceTitle, sequenceNumber, subjectDate, sequenceEpisodeTitle, type) => {
 	if (type === 'Terminu') return `Il-Kliem Fit-Teorija · ${title}`;
 	return [
-		subjectDate ? type : '',
+		subjectDate && title !== 'Bil-Moħbi fil-Beraħ' ? type : '',
 		type === 'Esej' && sequenceTitle,
-		subjectDate && fixSubjectDate(subjectDate) || sequenceNumber && (`#${ sequenceNumber }`),
+		subjectDate && fixSubjectDate(subjectDate) && title !== 'Bil-Moħbi fil-Beraħ' || sequenceNumber && (`#${ sequenceNumber }`),
 		sequenceEpisodeTitle || title,
 		"ta’",
 		author,
 		translator && `(tr ${translator})`
 	].filter(e => e).join(" ").replace(/:: /, ": ");
 }
+
+// When it's posssible to null diaryDate (subjectDate)
+//		subjectDate ? type : '',
