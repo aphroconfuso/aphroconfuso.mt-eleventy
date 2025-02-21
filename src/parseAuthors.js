d2297fd5232d41192a1e77eb680818467eb56c4e
Joe Gatt <joe@joegatt.net>
2025-02-21 10:30:43 +0100

    Fix assisted by authorsString

--- src/parseAuthors.js
+++ src/parseAuthors.js
@@ -3,7 +3,7 @@ module.exports = (authorsData, authorsType) => {
 	const authors = authorsData.map(author => author.attributes.displayName || `${ author.attributes.forename } ${ author.attributes.surname }`);
 	const authorForename = authorsData[0].attributes && authorsData[0].attributes.forename;
 	const authorSurname = authorsData[0].attributes && authorsData[0].attributes.surname;
-	const authorsString = authorsType === 'kollettiva' ? authors[0] : authors.join(", ");
+	const authorsString = authorsType === 'kollettiva' ? authors[0] : authorsType === 'assistita' ? `${authors[0]} ma’ ${authors.slice(1).join(", ")}` : authors.join(", ");
 	const author = authorsString;
 	const authorPronoun = authors.length > 1 ? 'huma' : authorsData[0].attributes.pronoun;
 	return { author, authors, authorForename, authorSurname, authorsString, authorPronoun };
