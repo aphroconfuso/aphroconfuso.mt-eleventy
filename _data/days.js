9cdfb9030097326570df36919a8e94a47c0221e1
Joe Gatt <joe@joegatt.net>
2025-04-06 00:10:47 +0200

    Add almanac feature

---
+++ _data/days.js
@@ -0,0 +1,20 @@
+const slugifyMaltese = require("../src/slugifyMaltese.js");
+const monthNames = require("../src/getMonthsInMaltese.js")();
+
+function days() {
+  const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // Frar always 29
+
+  const days = [];
+
+  for (let m = 0; m < 12; m++) {
+		for (let d = 1; d <= daysInMonth[m]; d++) {
+			const thisDate = `${ d } ta' ${ monthNames[m] }`;
+			console.log(slugifyMaltese(thisDate));
+      days.push({ date: thisDate });
+    }
+  }
+
+  return days;
+}
+
+module.exports = days();
