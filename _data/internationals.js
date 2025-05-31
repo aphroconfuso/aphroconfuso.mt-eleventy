307b6c018c5c2396073c42c6de8fe54ce1e878e7
Joe Gatt <joe@joegatt.net>
2025-05-31 15:55:53 +0200

    Update styles

--- _data/internationals.js
+++ _data/internationals.js
@@ -1,6 +1,10 @@
 const cachedPostFetch = require('../src/cachedPostFetch');
 const { linkedStoryDataWithImage } = require("./_fragments.js");
 
+const processPromos = require("../src/processPromos.js");
+const makeTitleSlug = require("../src/makeTitleSlug.js");
+const parseAuthors = require("../src/parseAuthors.js");
+
 const codes = {
 	"alerbyt": "ar",
 	"Catala": "ca",
@@ -31,18 +35,27 @@ async function getInternationals() {
 								language
 								title
 								body
-								story {
-									title
-									body
-									download
-									story {
-										${linkedStoryDataWithImage}
+								summaries {
+								download {
+									data {
+										attributes {
+											url
+											name
+											mime
+											size
+										}
 									}
+								}
+								text
+								title
+								story {
+									${ linkedStoryDataWithImage }
 								}
 							}
 						}
 					}
-				}`,
+				}
+			}`,
 			}),
 		});
 		const response = await data;
@@ -59,18 +72,27 @@ async function getInternationals() {
 		throw new Error(error);
 	}
 
-	const bookPromoFormatted = atts.bookPromos.length && processPromos(atts.bookPromos);
-
 	const internationalsFormatted = internationals.data.map((international) => {
 		const atts = international.attributes;
 		return {
+			body: atts.body,
 			code: codes[atts.language],
 			direction: (atts.language === 'alerbyt' ? 'rtl' : 'ltr'),
 			title: atts.title,
-			body: atts.body,
-		};
+		}
 	});
+
 	return internationalsFormatted;
 }
 
 module.exports = getInternationals;
+
+
+// summaries: atts.summaries.map((summary) => {
+// 	return {
+// 		text: summary.text,
+// 		title: summary.title,
+// 		download: summary.download && summary.download.data,
+// 		story: summary.story && summary.story.data && processPromos(summary.story.data),
+// 	}
+// })
